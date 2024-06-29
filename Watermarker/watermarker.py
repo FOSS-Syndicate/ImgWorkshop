82a5717001c9a15edcec9b146a782640dbbce04f
ShamalLakshan <shamallakshan00600@gmail.com>
2024-06-29 08:30:31 +0530

    watermarker-clean

--- Watermarker/watermarker.py
+++ Watermarker/watermarker.py
@@ -15,3 +15,21 @@ margin = 10
 size = (100, 100)
 watermark_image = Image.open("watermark.png")
 
+# to keep the aspect ration in intact
+watermark_image.thumbnail(size)
+
+# add watermark
+copied_image = image.copy()
+
+# Position
+x = x = width - 100 - margin
+y = height - 100 - margin
+
+
+# base image
+copied_image.paste(watermark_image, (x, y), watermark_image)
+
+copied_image.save('final.png')
+
+# pasted the crop image onto the base image
+plt.imshow(copied_image)

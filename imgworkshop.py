00daedd1e75b87a1007202a8b456bc0638cbe5fb
ShamalLakshan <shamallakshan00600@gmail.com>
2024-06-29 10:41:11 +0530

    main-menu

--- imgworkshop.py
+++ imgworkshop.py
@@ -1,23 +1,36 @@
-import swatcher
+def select_choice():
 
-print(r"  ___               __        __         _        _                 ")
-print(r" |_ _|_ __ ___   __\\ \      / /__  _ __| | _____| |__   ___  _ __  ")
-print(r"  | || '_ ` _ \ / _`\\ \ /\ / / _ \| '__| |/ / __| '_ \ / _ \| '_ \ ")
-print(r"  | || | | | | | (_| \\ V  V / (_) | |  |   <\__ \ | | | (_) | |_) |")
-print(r" |___|_| |_| |_|\__, |\\_/\_/ \___/|_|  |_|\_\___/_| |_|\___/| .__/ ")
-print(r"                |___/                                        |_|    ") 
+    while True:
+        print("1. Swatch Generation")
+        print("2. Watermarker")
+        print("3. Base64 Encoder")
+        user_input = input("Choose and operation: ")
+
+        if user_input == "1":
+            print("Swatcher/")  # To Link Script
+            break
+        elif user_input == "2":
+            print("Watermarker/") # To Link Script
+            break
+        elif user_input == "3":
+            print("Base64 Encoder/") # To Link Script
+            break
+        elif user_input == "q":
+            print("Bye Bye!!") # To Link Script
+            quit()
+        else:
+            print("Invalid arguement! Please choose a valid argument")
 
 
-print("1. Swatch Generation")
-print("2. Watermarker")
-print("3. Base64 Encoder")
-user_input = input("Choose and operation: ")
+def main():
+    print(r"  ___               __        __         _        _                 ")
+    print(r" |_ _|_ __ ___   __\\ \      / /__  _ __| | _____| |__   ___  _ __  ")
+    print(r"  | || '_ ` _ \ / _`\\ \ /\ / / _ \| '__| |/ / __| '_ \ / _ \| '_ \ ")
+    print(r"  | || | | | | | (_| \\ V  V / (_) | |  |   <\__ \ | | | (_) | |_) |")
+    print(r" |___|_| |_| |_|\__, |\\_/\_/ \___/|_|  |_|\_\___/_| |_|\___/| .__/ ")
+    print(r"                |___/                                        |_|    ") 
 
-if user_input == 1:
-    # Swatch function
-elif user_input == 2:
-    # Watermark function
-elif user_input == 3:
-    # base64 function
-else:
-    print("Invalid arguement! Please choose a valid argument")
+    select_choice()
+
+if __name__ == "__main__":
+    main()

d57373884f98369d89717762bf44d12538ef04c0
ShamalLakshan <shamallakshan00600@gmail.com>
2024-06-29 08:45:26 +0530

    code-clean

--- Watermarker/watermarker.py
+++ Watermarker/watermarker.py
@@ -1,15 +1,11 @@
 from PIL import Image
-import matplotlib.pyplot as plt
-import numpy as np
+
+
 
 # to open the image
 image = Image.open("base.jpg")
 width, height = image.size
 margin = 10
-
-# this open the photo viewer
-# image.show()
-#lt.imshow(image)
 
 # image watermark
 size = (100, 100)

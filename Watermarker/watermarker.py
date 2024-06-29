6b0082a1d9c2ee4997537fa07eab6f8bc6413aa9
ShamalLakshan <shamallakshan00600@gmail.com>
2024-06-29 08:10:42 +0530

    watermarker-base

--- Watermarker/watermarker.py
+++ Watermarker/watermarker.py
@@ -1,1 +1,17 @@
-# TODO
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+
+# to open the image
+image = Image.open("base.jpg")
+width, height = image.size
+margin = 10
+
+# this open the photo viewer
+# image.show()
+#lt.imshow(image)
+
+# image watermark
+size = (100, 100)
+watermark_image = Image.open("watermark.png")
+

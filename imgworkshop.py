94c222ef32b6a28cd35cdfe8a8002c08c98e6b46
ShamalLakshan <shamallakshan00600@gmail.com>
2024-06-29 18:43:00 +0530

    swatch-linked

--- imgworkshop.py
+++ imgworkshop.py
@@ -1,7 +1,26 @@
-import subprocess
+from PIL import Image
+
+
+def read_file(filename):
+    with open(filename,"r") as file:
+        for line in file:
+            params = line.split(":")
+            name, color = params[0], params[1]
+            print(name, "FFF" ,color)
+            create_image(name, color)
+
+
+def create_image(name, color):
+    im = Image.new("RGB", (50,50), color)
+    im.save(f"./swatch-output/{name}.png")
+
+
+def swatcher():
+    filename = input("Enter the location of the file containing all the color codes: ")
+    read_file(filename)
+
 
 def select_choice():
-
     while True:
         print("1. Swatch Generation")
         print("2. Watermarker")
@@ -10,7 +29,8 @@ def select_choice():
 
         if user_input == "1":
             print("Swatcher/")  # To Link Script
-            subprocess.call("./swatcher.py", shell=True)
+            swatcher()
+            break
         elif user_input == "2":
             print("Watermarker/") # To Link Script
             break
